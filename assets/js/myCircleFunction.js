30e891d157f1cd77c0cdf0bf12763acdb2f09fca
Marco M. von Hagen <M2vH@users.noreply.github.com>
2017-11-20 11:47:22 +0100

    Create myCircleFunction.js

---
+++ assets/js/myCircleFunction.js
@@ -0,0 +1,11 @@
+/* file: assets/js/myCircleFunction.js */
+function myCircleFunction_a() {
+  var x = document.getElementById("my_circle");
+    if (x.style.width === "100%") {
+      x.style.width = "50%";
+    } else {
+      x.style.width = "100%";
+    }    
+}
+console.log("assets/js/...");
+
